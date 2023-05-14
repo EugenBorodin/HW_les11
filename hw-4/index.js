59aaecf47c309bcc09b788830b1efa8b596ac645
Eugen Borodin <Azsalatay@gmail.com>
2023-05-14 15:26:29 +0200

    currency

---
+++ hw-4/index.js
@@ -0,0 +1,4 @@
+let currency = ["USD", "EUR", "KZT", "RUR", "GBR", "AED"];
+let newCurrency = currency.splice(1, 3, "I will earn big money by programming");
+console.log(currency);
+console.log(newCurrency);
