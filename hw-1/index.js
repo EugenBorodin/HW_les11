400b7d983155d75e9ac7fc5cb556caee2e16ac27
Eugen Borodin <Azsalatay@gmail.com>
2023-05-14 14:05:53 +0200

    add class Rose

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,25 +1,46 @@
 class Plant {
     constructor(height, age) {
-        this.height = height;
-        this.age =age;
+        this._height = height;
+        this._age = age;
     }
     get height() {
-        return this.height
+        return this._height
     }
 
     set height(value) {
-this.height = value;
+        this._height = value;
     }
 
     get age() {
-       return this.age;
+        return this._age;
     }
 
     set age(value) {
-        return this.age
+        this._age = value;
     }
 
     grow() {
-        this.height += 10;
+        this._height += 10;
     }
 }
+
+class Rose extends Plant {
+    constructor(height, age, numberOfFlowers) {
+        super(height, age);
+        this._numberOfFlowers = numberOfFlowers;
+    }
+
+    get numberOfFlowers() {
+        return this._numberOfFlowers;
+    }
+
+    set numberOfFlowers(value) {
+        this._numberOfFlowers = value;
+    }
+}
+
+const myRose = new Rose(20, 3, 5);
+console.log(myRose.height);
+myRose.grow();
+console.log(myRose.height);
+console.log(myRose.numberOfFlowers);
