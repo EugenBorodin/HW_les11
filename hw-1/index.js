81a5d077771e71c4472a739ec4da2eb6e9f5c6b2
Eugen Borodin <Azsalatay@gmail.com>
2023-05-14 13:56:34 +0200

    add get set

--- hw-1/index.js
+++ hw-1/index.js
@@ -3,4 +3,23 @@ class Plant {
         this.height = height;
         this.age =age;
     }
+    get height() {
+        return this.height
+    }
+
+    set height(value) {
+this.height = value;
+    }
+
+    get age() {
+       return this.age;
+    }
+
+    set age(value) {
+        return this.age
+    }
+
+    grow() {
+        this.height += 10;
+    }
 }
